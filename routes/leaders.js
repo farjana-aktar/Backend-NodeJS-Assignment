a5ad6ad749f45216901a508fe333901bb074bdcf
Farjana Aktar <69307558+farjana-aktar@users.noreply.github.com>
2022-03-09 15:07:47 +0600

    This is an assignment repo

--- routes/leaders.js
+++ routes/leaders.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
-const leadersRoutes = [
+const leaders = [
   { id: 1, name: "Farjana" },
   { id: 2, name: "Adrita" },
   { id: 3, name: "Chaity" },
